753bfc3cf5531f4be12685f4eef1c037f5ef4c57
cpa341 <cph2230@gmail.com>
2024-09-02 08:49:08 +0800

    Update install_wky.sh

--- install_wky.sh
+++ install_wky.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
-# 安装Git
+
+# 更新系统包列表
+echo "更新系统包列表..."
 sudo apt update
+
+# 安装Git
+echo "安装Git..."
 sudo apt install git -y
 
 # 删除旧的WKY-M目录
+echo "删除旧的WKY-M目录..."
 sudo rm -r WKY-M
 
-# 克隆新的WKY-M仓库
+# 克隆WKY-M仓库
+echo "克隆WKY-M仓库..."
 git clone https://github.com/cpa341/WKY-M
 
 # 创建firmware目录
+echo "创建firmware目录..."
 sudo mkdir /boot/firmware/
 
 # 复制PPPwn到firmware目录
+echo "复制PPPwn到firmware目录..."
 sudo cp -r WKY-M/PPPwn /boot/firmware/
 
 # 切换到PPPwn目录
-cd /boot/firmware/PPPwn
+cd /boot/firmware/PPPwn || exit
 
 # 给予权限
+echo "给予权限..."
 sudo chmod 777 *
 
 # 执行安装脚本
+echo "执行安装脚本..."
 sudo bash install_ch.sh
+
+echo "安装完成!"
